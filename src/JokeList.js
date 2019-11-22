98426860f293097c5baf868f73543eb95862171e
Liviu Preda <liviupreda@gmail.com>
2019-11-22 14:12:13 +0200

    get API joke from url on compDidMount; set numJokes to 10 as default prop; initialize state in JokeList.js

--- src/JokeList.js
+++ src/JokeList.js
@@ -1,9 +1,36 @@
+// GET https://icanhazdadjoke.com/
+// GET https://icanhazdadjoke.com/search
+
 import React, { Component } from "react";
 import axios from "axios";
 import Joke from "./Joke";
 import "./JokeList.css";
 
+let API_URL = "https://icanhazdadjoke.com/";
+
 class JokeList extends Component {
+  static defaultProps = {
+    numJokes: 10
+  };
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      jokes: []
+    };
+  }
+
+  componentDidMount() {
+    this.loadJokes();
+  }
+
+  async loadJokes() {
+    let res = await axios.get(API_URL, {
+      headers: { Accept: "application/json" }
+    });
+    console.log(res.data.joke);
+  }
+
   render() {
     return (
       <div className="JokeList">
