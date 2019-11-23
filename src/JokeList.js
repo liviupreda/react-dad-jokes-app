d284ac629f5df004ce5a0672d84ac3f56decdf8c
Liviu Preda <liviupreda@gmail.com>
2019-11-23 13:27:51 +0200

    add id to state, add keys to Joke comps

--- src/JokeList.js
+++ src/JokeList.js
@@ -12,7 +12,6 @@ class JokeList extends Component {
   static defaultProps = {
     numJokes: 10
   };
-
   constructor(props) {
     super(props);
     this.state = {
@@ -30,10 +29,14 @@ class JokeList extends Component {
       let res = await axios.get(API_URL, {
         headers: { Accept: "application/json" }
       });
-      jokes.push({ text: res.data.joke, votes: 0 });
+      jokes.push({ text: res.data.joke, votes: 0, id: res.data.id });
     }
 
     this.setState({ jokes });
+  }
+
+  handleVote(id, delta) {
+    // Handle upvotes and downvotes
   }
 
   render() {
@@ -43,12 +46,15 @@ class JokeList extends Component {
           <h1 className="JokeList-title">
             <span>Dad </span>Jokes
           </h1>
-          <img src="https://assets.dryicons.com/uploads/icon/svg/8927/0eb14c71-38f2-433a-bfc8-23d9c99b3647.svg" />
+          <img
+            src="https://assets.dryicons.com/uploads/icon/svg/8927/0eb14c71-38f2-433a-bfc8-23d9c99b3647.svg"
+            alt="Laughing Emoji"
+          />
           <button className="JokeList-getmore">Get More Jokes</button>
         </div>
         <div className="JokeList-jokes">
           {this.state.jokes.map(jk => (
-            <Joke votes={jk.votes} text={jk.text} />
+            <Joke key={jk.id} votes={jk.votes} text={jk.text} />
           ))}
         </div>
       </div>
